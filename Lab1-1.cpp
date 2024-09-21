56ac009f1007ea13b65ece61a467aba2ede4e949
Cheloveck322 <vasyagri2005@gmail.com>
2024-09-21 11:37:17 +0300

    Adding first lab1-1

---
+++ Lab1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    std::cout << "First string\n";
+    std::cout << "Первая строка\nВторая строка\n";
+    std::cout << '"' << char(92) << '"' << '\n';
+
+    return 0;
+}
