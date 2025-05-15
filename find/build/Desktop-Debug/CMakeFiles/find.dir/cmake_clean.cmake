4a633f56159fb31775f90a16b8b177b61da96ae6
Cheloveck322 <vasyagri2005@gmail.com>
2025-05-15 22:57:24 +0300

    adding find (lab2-1)

---
+++ find/build/Desktop-Debug/CMakeFiles/find.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find.dir/link.d"
+  "CMakeFiles/find_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/find_autogen.dir/ParseCache.txt"
+  "find_autogen"
+  "CMakeFiles/find.dir/FindDialog.cpp.o"
+  "CMakeFiles/find.dir/FindDialog.cpp.o.d"
+  "CMakeFiles/find.dir/find_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/find.dir/find_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/find.dir/main.cpp.o"
+  "CMakeFiles/find.dir/main.cpp.o.d"
+  "find"
+  "find.pdb"
+  "find_autogen/mocs_compilation.cpp"
+  "find_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ find/build/Desktop-Debug/CMakeFiles/find_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find_autogen"
+  "find_autogen/mocs_compilation.cpp"
+  "find_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/find_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
